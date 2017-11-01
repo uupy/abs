8225c77e54c623b761534f16ee570103a47214b3
TeemoPeng <danielpog@163.com>
2017-11-01 10:01:40 +0800

    --

--- src/pages/assets/property-to-verify.vue
+++ src/pages/assets/property-to-verify.vue
@@ -85,7 +85,7 @@
                 }                    
             },
             handleSelectionChange(){
- 
+                
             },
             clientsSizeChange(){
                 
